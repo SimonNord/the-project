1d5ca2e616e56a83283871d66d7e9049bf853368
Simon <simon.nordstrom@chasacademy.se>
2020-05-03 18:45:19 +0200

    Add expanded list  - Toggle when clicking arrow

--- src/components/Sidebar/components/UserCard/UserCard.tsx
+++ src/components/Sidebar/components/UserCard/UserCard.tsx
@@ -7,6 +7,13 @@ type Props = {
   user: User;
 };
 
+const container = {
+  display: "flex",
+  flexDirection: "column",
+  padding: "15px",
+  margin: "0px",
+  width: "200px",
+};
 const divStyles = {
   margin: "0px",
   display: "flex",
@@ -44,28 +51,49 @@ const arrowStyles = {
   justifySelf: "flex-end",
 };
 
+const expandedList = {
+  backgroundColor: "white",
+  textAlign: "center",
+};
+const expandedListText = {
+  fontSize: "15px",
+  lineHeight: "18px",
+  fontWeight: 500,
+  color: Color.CodGray,
+};
+
 const UserCard = (props: Props) => {
   let [show, setShow] = useState(false);
 
-  console.log(show);
-
   return (
-    <div style={divStyles}>
-      <img style={imageStyles} src="avatar-image.png" alt="avatar Image" />
-      <div style={textDiv}>
-        <p style={nameStyles}>{props.user.firstName}!</p>
-        <a style={emailStyles} href="#">
-          {props.user.email}
-        </a>
+    <div style={container}>
+      <div style={divStyles}>
+        <img style={imageStyles} src="avatar-image.png" alt="avatar Image" />
+        <div style={textDiv}>
+          <p style={nameStyles}>{props.user.firstName}!</p>
+          <a style={emailStyles} href="#">
+            {props.user.email}
+          </a>
+        </div>
+        <img
+          onClick={() => setShow((show = !show))}
+          style={arrowStyles}
+          src="./chevron-down.svg"
+          alt=""
+        />
       </div>
-      <img
-        onClick={() => setShow((show = !show))}
-        style={arrowStyles}
-        /* onClick={} */ src="./chevron-down.svg"
-        alt=""
-      />
+      {show ? (
+        <div style={expandedList}>
+          <p style={expandedListText}>Profile & Settings</p>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
+{
+  /* <expandedList show={show}/> */
+}
 
 export default UserCard;
