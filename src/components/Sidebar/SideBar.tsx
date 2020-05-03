6ba352bde729f39dd76066c88a64dedfbf66443d
Mo Sattler <mail@mosattler.com>
2020-05-03 10:22:00 +0200

    factor Sidebar out to own component

---
+++ src/components/Sidebar/SideBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "styled-components/macro"
+import { Color } from '../../variables'
+
+const SideBar = () => {
+    return (
+        <div css={`background-color: ${Color.Cinder};`}>
+          This will be the sidebar
+        </div>
+    )
+}
+
+export default SideBar
