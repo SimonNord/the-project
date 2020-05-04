d62b832a85caed722a17e81c93e8e48259aace57
Simon <simon.nordstrom@chasacademy.se>
2020-05-04 20:04:09 +0200

    Add styled-components on some elements

--- src/components/Sidebar/components/UserCard/UserCard.tsx
+++ src/components/Sidebar/components/UserCard/UserCard.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { User } from "types";
 import { Color } from "../../../../variables";
 import { withTheme } from "styled-components";
+import styled from "styled-components/macro";
 import { ReactComponent as ChevronDown } from "./assets/chevron-down.svg";
 import avatar from "./assets/avatar-image.png";
 
@@ -10,46 +11,46 @@ type Props = {
   user: User;
 };
 
-const container = {
-  display: "flex",
-  flexDirection: "column",
-  padding: "15px",
-  margin: "0px",
-  width: "200px",
-};
-const divStyles = {
-  margin: "0px",
-  display: "flex",
-  alignItems: "space-between",
-  backgroundColor: Color.LimedSpruce,
-  padding: "15px",
-  width: "200px",
-};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  margin: 0px;
+  width: 200px;
+`;
+const Div = styled.div`
+  margin: 0px;
+  display: flex;
+  align-items: space-between;
+  background-color: ${Color.LimedSpruce};
+  padding: 15px;
+  width: 200px;
+`;
 
-const nameStyles = {
-  color: Color.White,
-  margin: "0px",
-  fontSize: "15px",
-  lineHeight: "18px",
-  fontWeight: 600,
-};
+const Username = styled.p`
+  color: ${Color.White};
+  margin: 0px;
+  font-size: 15px;
+  line-height: 18px;
+  font-weight: 600;
+`;
 
-const emailStyles = {
-  fontSize: "13px",
-  lineHeight: "18px",
-  fontWeight: 500,
-  color: "hsla(0,0%,100%,.5)",
-};
+const Email = styled.a`
+  font-size: 13px;
+  line-height: 18px;
+  font-weight: 500;
+  color: hsla(0, 0%, 100%, 0.5);
+`;
 
-const textDiv = {
-  margin: "10px",
-};
+const TextContainer = styled.div`
+  margin: "10px";
+`;
 
-const imageStyles = {
-  width: "50px",
-  height: "50%",
-  borderRadius: "3px",
-};
+const Image = styled.img`
+  width: 50px;
+  height: 50%;
+  border-radius: 3px;
+`;
 
 const arrowStyles = {
   alignSelf: "center",
@@ -73,24 +74,22 @@ const UserCard = (props: Props) => {
   let [show, setShow] = useState(false);
 
   return (
-    <div style={container}>
-      <div style={divStyles}>
-        <img style={imageStyles} src={avatar} alt="" />
-        <div style={textDiv}>
-          <p style={nameStyles}>{props.user.firstName}!</p>
-          <a style={emailStyles} href="">
-            {props.user.email}
-          </a>
-        </div>
+    <Container>
+      <Div>
+        <Image src={avatar} alt="" />
+        <TextContainer>
+          <Username>{props.user.firstName}</Username>
+          <Email>{props.user.email}</Email>
+        </TextContainer>
         <ChevronDown style={arrowStyles} onClick={() => setShow(!show)} />
-      </div>
+      </Div>
       {show && (
         <div style={expandedList}>
           <p style={expandedListItem}>Profile & Settings</p>
           <p style={expandedListItem}> Sign Out</p>
         </div>
       )}
-    </div>
+    </Container>
   );
 };
 
