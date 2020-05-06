0e435af633a5e6d179da49750075e1fc4f698d2b
Mo Sattler <mail@mosattler.com>
2020-05-06 14:24:59 +0200

    Update src/components/Sidebar/components/SectionHeadline/SectionHeadline.tsx

--- src/components/Sidebar/components/SectionHeadline/SectionHeadline.tsx
+++ src/components/Sidebar/components/SectionHeadline/SectionHeadline.tsx
@@ -5,16 +5,6 @@ type Props = {
   children: ReactNode;
 };
 
-// Getting an error when trying to use textTransform property
-const sectionHeadStyles = {
-  fontWeight: 500,
-  // textTransform: '',
-  fontSize: "13px",
-  color: Color.GreySeeThru,
-  letterSpacing: 0,
-  padding: "11px 15px",
-};
-
 const SectionHeadline = ({ children }: Props) => {
   return <div style={{
   fontWeight: 500,
