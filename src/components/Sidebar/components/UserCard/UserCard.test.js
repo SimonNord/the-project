52de3b635d622dac01b8145062c0889e1146831f
Simon <simon.nordstrom@chasacademy.se>
2020-05-07 16:11:29 +0200

    Add test for UserCard  - should render the first name  - should render the email  - should expand when being clicked  - should collapse when being clicked again

--- src/components/Sidebar/components/UserCard/UserCard.test.js
+++ src/components/Sidebar/components/UserCard/UserCard.test.js
@@ -3,25 +3,24 @@ import { render } from "@testing-library/react";
 import UserCard from "./UserCard";
 import userEvent from "@testing-library/user-event";
 
+const testUser = {
+  firstName: "John",
+  lastName: "Doe",
+  email: "john@doe.com",
+};
 it("should render the first name", () => {
-  const testUser = {
-    firstName: "John",
-    lastName: "Doe",
-    email: "john@doe.com",
-  };
   const { queryByText } = render(<UserCard user={testUser} />);
   const firstName = queryByText(testUser.firstName);
   expect(firstName).toBeInTheDocument();
 });
 
-it.todo("should render the email");
+it("should render the email", () => {
+  const { queryByText } = render(<UserCard user={testUser} />);
+  const email = queryByText(testUser.email);
+  expect(email).toBeInTheDocument();
+});
 
 it("should expand when being clicked", () => {
-  const testUser = {
-    firstName: "John",
-    lastName: "Doe",
-    email: "john@doe.com",
-  };
   const { queryByText } = render(<UserCard user={testUser} />);
 
   expect(queryByText("Profile & Settings")).not.toBeInTheDocument(); // not expanded yet, should be invisible
@@ -29,4 +28,14 @@ it("should expand when being clicked", () => {
   expect(queryByText("Profile & Settings")).toBeInTheDocument(); // now it should be visible
 });
 
-it.todo("should collapse when clicked again");
+it("should collapse when clicked again", () => {
+  const { queryByText } = render(<UserCard user={testUser} />);
+
+  expect(queryByText("Profile & Settings")).not.toBeInTheDocument(); // not expanded yet, should be invisible
+
+  userEvent.click(queryByText(testUser.firstName)); // now I clicked on the card (in this case firstName, but could have been anything else)
+  expect(queryByText("Profile & Settings")).toBeInTheDocument(); // now it should be visible
+
+  userEvent.click(queryByText(testUser.firstName)); // now I clicked on the card (in this case firstName, but could have been anything else)
+  expect(queryByText("Profile & Settings")).not.toBeInTheDocument(); // collapsed again, should be invisible
+});
