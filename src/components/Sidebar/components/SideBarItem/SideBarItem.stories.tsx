51d5f25da3620f2bacf394f19741465d8bf79083
Mo Sattler <mail@mosattler.com>
2020-05-07 15:39:50 +0200

    Dont mess with the story :)

--- src/components/Sidebar/components/SideBarItem/SideBarItem.stories.tsx
+++ src/components/Sidebar/components/SideBarItem/SideBarItem.stories.tsx
@@ -8,64 +8,43 @@ export default {
 
 export const SideBarItemStory = () => (
   <div>
-    <SideBarItem
-      css={`
-        background-color: #24292e;
-        color: white;
-        display: table-cell;
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 15px;
-        font-weight: 500;
-        line-height: 18px;
-        padding: 11px 15px;
-        vertical-align: middle;
-
-        &:hover {
-          background-color: #0366d6;
-        }
-      `}
-    >
-      Progress
+    <SideBarItem>
+      <span
+        css={`
+          font-weight: 500;
+          font-size: 15px;
+          color: black;
+          line-height: 18px;
+        `}
+      >
+        Progress
+      </span>
     </SideBarItem>
     <div css="padding-top: 64px;" />
-    <SideBarItem
-      css={`
-        background-color: #24292e;
-        color: white;
-        display: table-cell;
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 15px;
-        font-weight: 500;
-        line-height: 18px;
-        padding: 11px 15px;
-        vertical-align: middle;
-
-        &:hover {
-          background-color: #0366d6;
-        }
-      `}
-    >
-      Info
+    <SideBarItem>
+      <span
+        css={`
+          font-weight: 500;
+          font-size: 15px;
+          color: black;
+          line-height: 18px;
+        `}
+      >
+        Info
+      </span>
     </SideBarItem>
     <div css="padding-top: 64px;" />
-    <SideBarItem
-      css={`
-        background-color: #24292e;
-        color: white;
-        display: table-cell;
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 15px;
-        font-weight: 500;
-        line-height: 18px;
-        padding: 11px 15px;
-        vertical-align: middle;
-
-        &:hover {
-          background-color: #0366d6;
-        }
-      `}
-    >
-      User
+    <SideBarItem>
+      <span
+        css={`
+          font-weight: 500;
+          font-size: 15px;
+          color: black;
+          line-height: 18px;
+        `}
+      >
+        User
+      </span>
     </SideBarItem>
   </div>
 );
