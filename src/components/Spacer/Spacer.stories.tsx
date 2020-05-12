147cee26cff191e6f5a6cdb8eaa0664e6e3e54af
ruaridh mcguckin <ruaridh.mcguckin@gmail.com>
2020-05-12 23:34:15 +0100

    Added consts to stop mutating state

--- src/components/Spacer/Spacer.stories.tsx
+++ src/components/Spacer/Spacer.stories.tsx
@@ -5,15 +5,11 @@ export default {
   title: "Spacer",
 };
 
-export const SpacerStory = () => {
-  return (
-    <div>
-      <p>display text</p>
-      <Spacer top={4} />
-      <p>display text</p>
-    </div>
-  );
-};
+export const SpacerStory = () => (
+  <div style={{ border: "1px solid red" }}>
+    <Spacer top={4} left={4} />
+  </div>
+);
 
 SpacerStory.story = {
   name: "default",
--- src/components/Spacer/Spacer.tsx
+++ src/components/Spacer/Spacer.tsx
@@ -8,21 +8,25 @@ type Props = {
 };
 
 const Spacer = ({ top, right, bottom, left }: Props) => {
-  top *= 8;
-  right *= 8;
-  bottom *= 8;
-  left *= 8;
+  const paddingTop = top * 8;
+  const paddingRight = right * 8;
+  const paddingBottom = bottom * 8;
+  const paddingLeft = left * 8;
 
   return (
-    <div style={{ padding: `${top}px ${right}px ${bottom}px ${left}px` }}></div>
+    <div
+      style={{
+        padding: `${paddingTop}px ${paddingRight}px ${paddingBottom}px ${paddingLeft}px`,
+      }}
+    ></div>
   );
 };
 
 Spacer.defaultProps = {
-  top: "0",
-  right: "0",
-  bottom: "0",
-  left: "0",
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
 };
 
 export default Spacer;
