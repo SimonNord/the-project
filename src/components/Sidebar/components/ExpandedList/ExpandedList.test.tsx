2739d7806ce42c707e7e9027202340d1eeeedcf0
Liam Burns <liamburns@gmail.com>
2020-05-14 13:40:41 +0100

    Removes unnecessary comments from ExpandedList.test

--- src/components/Sidebar/components/ExpandedList/ExpandedList.test.tsx
+++ src/components/Sidebar/components/ExpandedList/ExpandedList.test.tsx
@@ -2,15 +2,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ExpandedList, { ExpandedListItem } from "./ExpandedList";
 
-/*
-// jest.it and jest.test are aliases of the same function
-// But typically you would use it as per the example below
-// whereas test would be used if you word the function as follows:
-// test("<ExpandedList", () => {
-// // Test goes here  
-// });
-// Useful link https://blog.sapegin.me/all/react-testing-3-jest-and-react-testing-library/
-*/
 it("should render the list items", () => {
   const { queryByText } = render(
     <ExpandedList>
