fa705b0cb4f5f008e834d4bf66248406391004c8
Simon <simon.nordstrom@chasacademy.se>
2020-05-17 22:00:11 +0200

    Add Spacer in ProfileAndSettings

--- src/components/Content/components/ProfileAndSettings/ProfileAndSettings.tsx
+++ src/components/Content/components/ProfileAndSettings/ProfileAndSettings.tsx
@@ -5,6 +5,7 @@ import SettingsMenu from "./components/SettingsMenu";
 import General from "./components/General";
 import AppSettings from "./components/AppSettings";
 import Password from "./components/Password";
+import Spacer from "components/Spacer/Spacer";
 
 const ProfileSettingsContainer = styled.div`
   display: flex;
@@ -13,20 +14,22 @@ const ProfileSettingsContainer = styled.div`
 
 const ProfileAndSettings = () => {
   return (
-    <ProfileSettingsContainer>
-      <SettingsMenu />
-      <Switch>
-        <Route path="/settings/general">
-          <General />
-        </Route>
-        <Route path="/settings/app-settings">
-          <AppSettings />
-        </Route>
-        <Route path="/settings/password">
-          <Password />
-        </Route>
-      </Switch>
-    </ProfileSettingsContainer>
+    <Spacer top={2} right={2} bottom={2} left={2}>
+      <ProfileSettingsContainer>
+        <SettingsMenu />
+        <Switch>
+          <Route path="/settings/general">
+            <General />
+          </Route>
+          <Route path="/settings/app-settings">
+            <AppSettings />
+          </Route>
+          <Route path="/settings/password">
+            <Password />
+          </Route>
+        </Switch>
+      </ProfileSettingsContainer>
+    </Spacer>
   );
 };
 
