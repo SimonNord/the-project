f660c0550d5c4b5e0cf6cfca31a046ca6a11f413
Liam Burns <liamburns@gmail.com>
2020-05-19 19:56:02 +0100

    Adds an enum Routes to variables Routing

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components/macro";
 import NoContentPlaceholder from "./components/NoContentPlaceholder/NoContentPlaceholder";
 import ProfileAndSettings from "./components/ProfileAndSettings";
 import { Switch, Route } from "react-router-dom";
+import { Routes } from "variables";
 
 type Props = {
   className?: string;
@@ -18,10 +19,10 @@ const Content = (props: Props) => {
   return (
     <FullHeightAndWidth className={props.className}>
       <Switch>
-        <Route path="/settings">
+        <Route path={Routes.Settings}>
           <ProfileAndSettings />
         </Route>
-        <Route path="/">
+        <Route path={Routes.Home}>
           <NoContentPlaceholder />
         </Route>
       </Switch>
