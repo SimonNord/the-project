d2cc8c13931a27ad294309622d89d3fa4c05ec1a
Liam Burns <liamburns@gmail.com>
2020-05-22 07:49:05 +0100

    Adds a test for the Sidebar markup

---
+++ src/components/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+
+import Sidebar from "./Sidebar";
+import UserCard from "./components/UserCard/UserCard";
+
+// A test user for all the following tests
+const testUser = {
+  firstName: "John",
+  lastName: "Doe",
+  email: "john@doe.com",
+};
+
+// Test for a UserCard
+test("should render a Sidebar with a UserCard", () => {
+  render(<Sidebar user={testUser} />);
+  const firstName = screen.queryByText(testUser.firstName);
+  expect(firstName).toBeInTheDocument();
+});
+
+// Test for the presence of each menu item
+test.each([
+  ["Afternoon"],
+  ["All Habits"],
+  ["Progress"],
+  ["Manage Habits"],
+  ["Resources"],
+])("should render a Sidebar with a link to %s", (menuItem) => {
+  render(<Sidebar user={testUser} />);
+
+  const menuItemResult = screen.queryByText(menuItem);
+  expect(menuItemResult).toBeInTheDocument();
+});
+
+// Test for the presence of the sidebar footer button
+test("should render a Sidebar with an Add New button", () => {
+  render(<Sidebar user={testUser} />);
+  const footerButton = screen.queryByText("Add New...");
+  expect(footerButton).toBeInTheDocument();
+});
