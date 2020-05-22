47d61c4af992edcc086a49486938cc11e197c909
Liam Burns <limbovski@users.noreply.github.com>
2020-05-22 09:25:53 +0100

    Renames Routes enum to RoutePath (#204)

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components/macro";
 import NoContentPlaceholder from "./components/NoContentPlaceholder/NoContentPlaceholder";
 import ProfileAndSettings from "./components/ProfileAndSettings";
 import { Switch, Route } from "react-router-dom";
-import { Routes } from "variables";
+import { RoutePath } from "variables";
 
 type Props = {
   className?: string;
@@ -19,10 +19,10 @@ const Content = (props: Props) => {
   return (
     <FullHeightAndWidth className={props.className}>
       <Switch>
-        <Route path={Routes.Settings}>
+        <Route path={RoutePath.Settings}>
           <ProfileAndSettings />
         </Route>
-        <Route path={Routes.Home}>
+        <Route path={RoutePath.Home}>
           <NoContentPlaceholder />
         </Route>
       </Switch>
