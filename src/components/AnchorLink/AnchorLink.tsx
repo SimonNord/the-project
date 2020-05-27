b44f75bd59f40634eb6c08e4c946d736790226ba
Liam Burns <liamburns@gmail.com>
2020-05-27 20:25:17 +0100

    Tidies up CSS for AnchorLink removing styled components for css

--- src/components/AnchorLink/AnchorLink.tsx
+++ src/components/AnchorLink/AnchorLink.tsx
@@ -6,32 +6,30 @@ import { Color, RoutePath } from "variables";
 
 type Props = {
   url: RoutePath;
-  children: ReactNode;
   linkColor?: Color.White | Color.Black;
+  children: ReactNode;
 };
 
-// Extend the <Link> component with customised styles
-// color is passed by prop
-const StyledLink = styled(Link).attrs({
-  role: "menuitem",
-})`
-  display: flex;
-  align-items: center;
-  padding: 12px 15px;
-  text-decoration: none;
-  transition: color 0.2s ease-in-out, background-color 0.2s ease-in-out;
-
-  &:hover {
-    color: ${Color.White};
-    background-color: ${Color.DodgerBlue};
-  }
-`;
-
-export const AnchorLink = ({ url, children, linkColor }: Props) => {
+export const AnchorLink = ({ url, linkColor, children }: Props) => {
   return (
-    <StyledLink to={url} style={{ color: linkColor }}>
+    <Link
+      to={url}
+      css={`
+        display: flex;
+        align-items: center;
+        color: ${linkColor};
+        padding: 12px 15px;
+        text-decoration: none;
+        transition: color 0.2s ease-in-out, background-color 0.2s ease-in-out;
+
+        &:hover {
+          color: ${Color.White};
+          background-color: ${Color.DodgerBlue};
+        }
+      `}
+    >
       {children}
-    </StyledLink>
+    </Link>
   );
 };
 
