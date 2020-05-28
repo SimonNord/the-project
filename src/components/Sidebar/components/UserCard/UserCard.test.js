93ac7f3093faec3bb47197ade0875efc6ebde08c
Liam Burns <liamburns@gmail.com>
2020-05-28 16:24:57 +0100

    Replaces queryByRole with queryByText in UserCard test

--- src/components/Sidebar/components/UserCard/UserCard.test.js
+++ src/components/Sidebar/components/UserCard/UserCard.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import UserCard from './UserCard';
-import userEvent from '@testing-library/user-event';
-import { MemoryRouter } from 'react-router-dom';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import UserCard from "./UserCard";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
 
-it('should render the first name', () => {
+it("should render the first name", () => {
   const testUser = {
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'john@doe.com',
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@doe.com",
   };
 
   render(<UserCard user={testUser} />);
@@ -16,11 +16,11 @@ it('should render the first name', () => {
   expect(firstName).toBeInTheDocument();
 });
 
-it('should render the email', () => {
+it("should render the email", () => {
   const testUser = {
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'john@doe.com',
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@doe.com",
   };
 
   render(<UserCard user={testUser} />);
@@ -28,11 +28,11 @@ it('should render the email', () => {
   expect(email).toBeInTheDocument();
 });
 
-it('should expand when being clicked', () => {
+it("should expand when being clicked", () => {
   const testUser = {
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'john@doe.com',
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@doe.com",
   };
 
   render(
@@ -41,20 +41,16 @@ it('should expand when being clicked', () => {
     </MemoryRouter>
   );
 
-  expect(
-    screen.queryByRole('menuitem', { name: 'Profile & Settings' })
-  ).not.toBeInTheDocument(); // not expanded yet, should be invisible
+  expect(screen.queryByText("Profile & Settings")).not.toBeInTheDocument(); // not expanded yet, should be invisible
   userEvent.click(screen.queryByText(testUser.firstName)); // now I clicked on the card (in this case firstName, but could have been anything else)
-  expect(
-    screen.queryByRole('menuitem', { name: 'Profile & Settings' })
-  ).toBeInTheDocument(); // now it should be visible
+  expect(screen.queryByText("Profile & Settings")).toBeInTheDocument(); // now it should be visible
 });
 
-it('should collapse when clicked again', () => {
+it("should collapse when clicked again", () => {
   const testUser = {
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'john@doe.com',
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@doe.com",
   };
 
   render(
@@ -63,11 +59,11 @@ it('should collapse when clicked again', () => {
     </MemoryRouter>
   );
 
-  expect(screen.queryByText('Profile & Settings')).not.toBeInTheDocument(); // not expanded yet, should be invisible
+  expect(screen.queryByText("Profile & Settings")).not.toBeInTheDocument(); // not expanded yet, should be invisible
 
   userEvent.click(screen.queryByText(testUser.firstName)); // now I clicked on the card (in this case firstName, but could have been anything else)
-  expect(screen.queryByText('Profile & Settings')).toBeInTheDocument(); // now it should be visible
+  expect(screen.queryByText("Profile & Settings")).toBeInTheDocument(); // now it should be visible
 
   userEvent.click(screen.queryByText(testUser.firstName)); // now I clicked on the card (in this case firstName, but could have been anything else)
-  expect(screen.queryByText('Profile & Settings')).not.toBeInTheDocument(); // collapsed again, should be invisible
+  expect(screen.queryByText("Profile & Settings")).not.toBeInTheDocument(); // collapsed again, should be invisible
 });
