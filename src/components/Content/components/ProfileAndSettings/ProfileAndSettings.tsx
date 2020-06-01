7edc8303765bf393322a68a709437273a2eac2c9
Simon <simon.nordstrom@chasacademy.se>
2020-06-01 02:30:41 +0200

    add page title

--- src/components/Content/components/ProfileAndSettings/ProfileAndSettings.tsx
+++ src/components/Content/components/ProfileAndSettings/ProfileAndSettings.tsx
@@ -12,24 +12,32 @@ const ProfileSettingsContainer = styled.div`
   flex-direction: row;
 `;
 
+const PageTitle = styled.div`
+  padding: 16px;
+  border: 1px solid #e5eafa;
+`;
+
 const ProfileAndSettings = () => {
   return (
-    <Spacer top={2} right={2} bottom={2} left={2}>
-      <ProfileSettingsContainer>
-        <SettingsMenu />
-        <Switch>
-          <Route path="/settings/general">
-            <General />
-          </Route>
-          <Route path="/settings/app-settings">
-            <AppSettings />
-          </Route>
-          <Route path="/settings/password">
-            <Password />
-          </Route>
-        </Switch>
-      </ProfileSettingsContainer>
-    </Spacer>
+    <div>
+      <PageTitle>Profile And Settings</PageTitle>
+      <Spacer top={2} right={2} bottom={2} left={2}>
+        <ProfileSettingsContainer>
+          <SettingsMenu />
+          <Switch>
+            <Route path="/settings/general">
+              <General />
+            </Route>
+            <Route path="/settings/app-settings">
+              <AppSettings />
+            </Route>
+            <Route path="/settings/password">
+              <Password />
+            </Route>
+          </Switch>
+        </ProfileSettingsContainer>
+      </Spacer>
+    </div>
   );
 };
 
