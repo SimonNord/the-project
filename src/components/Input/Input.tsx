f4291538b19da94fc486abae87a45988834ccff4
Simon <simon.nordstrom@chasacademy.se>
2020-06-03 13:27:52 +0200

    WIP add files

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const StyledInput = styled.input`
+  border-radius: 8px;
+`;
+
+const Input = () => {
+  return <StyledInput></StyledInput>;
+};
+
+export default Input;
