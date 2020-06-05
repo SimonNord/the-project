27d42d5ee458d89bc378b3f7b0e3cf311a98925c
Simon <simon.nordstrom@chasacademy.se>
2020-06-05 11:39:42 +0200

    Fix test errors  - import Switch and Route from react-router-dom instead

--- src/components/Content/components/ProfileAndSettings/ProfileAndSettings.tsx
+++ src/components/Content/components/ProfileAndSettings/ProfileAndSettings.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router";
+import { Switch, Route } from "react-router-dom";
 import styled from "styled-components";
 import SettingsMenu from "./components/SettingsMenu";
 import General from "./components/General";
